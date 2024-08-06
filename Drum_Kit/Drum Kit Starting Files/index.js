376dc6678e819bcb2d56ca531c466024e2cfc1c3
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-08-06 21:55:41 +0600

    Drum kit file updated

--- Drum_Kit/Drum Kit Starting Files/index.js
+++ Drum_Kit/Drum Kit Starting Files/index.js
@@ -1,1 +1,10 @@
 // alert("Hello");
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert('I got clicked');
+        }
+    );
+}
