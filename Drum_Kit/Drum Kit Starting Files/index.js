bc63a5dc07c940e9b3d9d14b92f313f3a88bb5a7
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-08-07 03:51:26 +0600

    keyboard connection with drum added

--- Drum_Kit/Drum Kit Starting Files/index.js
+++ Drum_Kit/Drum Kit Starting Files/index.js
@@ -7,45 +7,52 @@ for(var i=0; i<numberOfDrumButtons; i++){
             
             var buttonInnerHTML = this.innerHTML;
 
-            //w,a,s,d,j,k,l
-            switch(buttonInnerHTML){
-                case "w":
-                    var tom_1 = new Audio("sounds/tom-1.mp3");
-                    tom_1.play();
-                    break;
-                case "a":
-                    var tom_2 = new Audio("sounds/tom-2.mp3");
-                    tom_2.play();
-                    break;
-                case "s":
-                    var tom_3 = new Audio("sounds/tom-3.mp3");
-                    tom_3.play();
-                    break;    
-                case "d":
-                    var tom_4 = new Audio("sounds/tom-4.mp3");
-                    tom_4.play();
-                    break;
-                case "j":
-                    var snare = new Audio("sounds/snare.mp3");
-                    snare.play();
-                    break;
-                case "k":
-                    var crash = new Audio("sounds/crash.mp3");
-                    crash.play();
-                    break;  
-                case "l":
-                    var kick = new Audio("sounds/kick-bass.mp3");
-                    kick.play();
-                    break;                         
-                default:
-                    console.log(buttonInnerHTML);
-                
-            }
+            makeSound(buttonInnerHTML);
+            
         }
     );
 }
 
 
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+});
 
 
+function makeSound(key){
 
+    //w,a,s,d,j,k,l
+    switch(key){
+        case "w":
+            var tom_1 = new Audio("sounds/tom-1.mp3");
+            tom_1.play();
+            break;
+        case "a":
+            var tom_2 = new Audio("sounds/tom-2.mp3");
+            tom_2.play();
+            break;
+        case "s":
+            var tom_3 = new Audio("sounds/tom-3.mp3");
+            tom_3.play();
+            break;    
+        case "d":
+            var tom_4 = new Audio("sounds/tom-4.mp3");
+            tom_4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;  
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;                         
+        default:
+            console.log(buttonInnerHTML);
+        
+    }
+}
